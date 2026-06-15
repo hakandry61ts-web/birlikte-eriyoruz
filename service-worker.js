f5b9a34ccc8069b8c216f048528ff0f6d9c434da
hakandry61ts-web <hakandry61ts@gmail.com>
2026-06-15 10:49:22 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,16 @@
-const CACHE_NAME = "birlikte-eriyoruz-v1-3";
-const ASSETS = ["./manifest.json", "./icon-192.png", "./icon-512.png"];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+      Promise.all(keys.map((key) => caches.delete(key)))
+    ).then(() => self.registration.unregister())
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  const requestUrl = new URL(event.request.url);
-
-  if (requestUrl.pathname.endsWith("/") || requestUrl.pathname.endsWith("/index.html")) {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-          return response;
-        })
-        .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-        return response;
-      });
-    })
-  );
+  return;
 });
